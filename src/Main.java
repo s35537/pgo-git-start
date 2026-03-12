842bdd3d2878c8a5c01552331f0ce797303e303c
Kajetan <s35537@pjwstk.edu.pl>
2026-03-12 18:54:23 +0100

    Dodanie pomocniczej informacji o zadaniu

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ Zadanie 1 Java, s35537
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projekt PGO");
+        System.out.println("Ćwiczenia 1 PGO - Zad 4");
         System.out.println("Autor: s35537");
         System.out.println("PJATK 2026");
     }
